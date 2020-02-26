d497466e11cc91bc4ff4efe14dea5a5575b8124e
sichu366 <sichu@ucsc.edu>
2020-02-26 15:20:49 -0600

    simulate all network

--- Interval-based-method/main_program_interval.m
+++ Interval-based-method/main_program_interval.m
@@ -16,44 +16,53 @@ case_study = inpNameIndex;
 %Load data
 switch case_study
     case 1
-%         load('3-node-Lipschtiz.mat');
+        %         load('3-node-Lipschtiz.mat');
         %Epsilons for stopping criteria
         eps_X = [10^-3];
         eps_F = [1*10^-2];
         max_iter = Inf;
+        str_case = '3_node.mat';
     case 2
-%         load('8-node-Lipschtiz.mat');
+        %         load('8-node-Lipschtiz.mat');
         %Epsilons for stopping criteria
         eps_X = [10^-3];
         eps_F = [1*10^-2];
         max_iter = Inf;
+        str_case = '8_node.mat';
     case 3
-%         load('anytown-Lipschtiz.mat');
+        %         load('anytown-Lipschtiz.mat');
         %Epsilons for stopping criteria
         eps_X = [10^-3];
         eps_F = [1*10^0];
         max_iter = Inf;
+        str_case = 'Anytown.mat';
     case 4
-%         load('net2-Lipschtiz.mat');
+        %         load('net2-Lipschtiz.mat');
         %Epsilons for stopping criteria
         eps_X = [10^-3];
         eps_F = [1*10^-5];
         max_iter = Inf;
+        str_case = 'Net2.mat';
     case 5
-%         load('net3-Lipschtiz.mat');
+        %         load('net3-Lipschtiz.mat');
         %Epsilons for stopping criteria
         eps_X = [10^-3];
         eps_F = [1*10^-2];
         max_iter = Inf;
+        str_case = 'Net3.mat';
     case 6
-%         load('OBCL-Lipschtiz.mat');
+        %         load('OBCL-Lipschtiz.mat');
         %Epsilons for stopping criteria
         eps_X = [10^-3];
         eps_F = [1*10^-1];
         max_iter = Inf;
+        str_case = 'OBCL.mat';
     otherwise
         disp('other value');
+        str_case = 'other value';
 end
+
+
 
 %Set constant data
 param.mu = mu;
@@ -146,12 +155,16 @@ for i = 1:length(eps_F)
     Tab1 = cell2table(data1_tab,'VariableNames',{'index','eps_F', 'eps_X', 'gamma','comp_time','total_split','total_split1','total_split2','total_subset','gap','is_optimal'});
     
     %Save file
-    filename = 'data_experiment_tab_case_2.mat';
+    %filename = 'data_experiment_tab_case_2.mat';
+    
+    filename = strcat('data_experiment_tab','_',str_case);
+
     save(filename,'Tab1');
     
     %Save data
     exp_data1.result{i} = {i, eps_F(i), eps_X(i), Lip, totalTime, result, param};
-    save('data_experiment_case_2.mat','exp_data1');
+    filename = strcat('data_experiment','_',str_case);
+    save(filename,'exp_data1');
     
 end
 
--- main.m
+++ main.m
@@ -2,48 +2,55 @@ clc;
 clear;
 close all;
 
-inpNameIndex = 3;
-switch inpNameIndex
-    case 1
-        disp('Threenodes.inp');
-    case 2
-        disp('tutorial8node.inp');
-    case 3
-        disp('Anytown.inp');
-    case 4
-        disp('Net2.inp');
-    case 5
-        disp('Net3.inp');
-    case 6
-        disp('OBCL.inp');
-    otherwise
-        disp('other value');
+
+for  inpNameIndex = 1:1
+    switch inpNameIndex
+        case 1
+            disp('Threenodes.inp');
+        case 2
+            disp('tutorial8node.inp');
+        case 3
+            disp('Anytown.inp');
+        case 4
+            disp('Net2.inp');
+        case 5
+            disp('Net3.inp');
+        case 6
+            disp('OBCL.inp');
+        otherwise
+            disp('other value');
+    end
+    
+    %% Find Lipschitz via anlytical solution
+    FindLipschitz_v2
+    
+    %% Find Lipschitz via interval algebra
+    main_program_interval
+    
+    % the result is printed and stored in variable: Lip
+    
+    %% Find Lipschitz via point based (max mode and sqrt mode)
+    % note that 1) the max mode should be close to anlytical solution 2) the
+    % sqrt mode should be close to interval algebra result
+    
+    for j = 1:2
+        if (j==1)
+            mode_point_based = 'sqrt';
+        else
+            mode_point_based = 'max';
+        end
+        main_program_point_based
+    end
+    
+    
+    
+    % the final result is stored in the following cell
+    
+    % vmean_f_h
+    % vmean_f_r
+    % vmean_f_s
+    
+    
 end
-
-%% Find Lipschitz via anlytical solution
-FindLipschitz_v2
-
-%% Find Lipschitz via interval algebra
-main_program_interval
-
-% the result is printed and stored in variable: Lip
-
-%% Find Lipschitz via point based (max mode and sqrt mode)
-% note that 1) the max mode should be close to anlytical solution 2) the
-% sqrt mode should be close to interval algebra result
-
-
-mode_point_based = 'sqrt';
-%mode_point_based = 'max';
-
-main_program_point_based
-
-
-% the final result is stored in the following cell 
-
-% vmean_f_h
-% vmean_f_r
-% vmean_f_s
-
 
 
